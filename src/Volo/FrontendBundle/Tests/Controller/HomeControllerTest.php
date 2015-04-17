bb3d40c6b76869939ba9f4f2b08407b96d890cb7
steffen.kraft <steffen.kraft@rocket-internet.de>
2015-04-17 11:53:27 +0200

    HomeController Test - wrong class name

--- src/Volo/FrontendBundle/Tests/Controller/HomeControllerTest.php
+++ src/Volo/FrontendBundle/Tests/Controller/HomeControllerTest.php
@@ -4,7 +4,7 @@ namespace Volo\FrontendBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class HomeControllerTest extends WebTestCase
 {
     public function testIndex()
     {
