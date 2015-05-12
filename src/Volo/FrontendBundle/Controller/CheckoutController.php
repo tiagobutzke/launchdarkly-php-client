c92521779fd01888d89c9596fab572dae7309be0
mohamed.shehab <mohamed.shehab@rocket-internet.de>
2015-05-12 15:49:03 +0200

    SGFD-15488: Cart-Integration into checkout

--- src/Volo/FrontendBundle/Controller/CheckoutController.php
+++ src/Volo/FrontendBundle/Controller/CheckoutController.php
@@ -5,15 +5,28 @@ namespace Volo\FrontendBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class CheckoutController extends Controller
 {
     /**
      * @Route("/checkout/{vendorCode}", name="checkout")
      * @Template()
+     *
+     * @param Request $request
+     * @param $vendorCode
+     *
+     * @return array
      */
-    public function indexAction($vendorCode)
+    public function indexAction(Request $request, $vendorCode)
     {
-        
+        return [
+            'cart' => json_encode(
+                $this->get('volo_frontend.service.cart_manager')->getCart(
+                    $request->getSession()->getId(),
+                    $vendorCode
+                )
+            )
+        ];
     }
 }
