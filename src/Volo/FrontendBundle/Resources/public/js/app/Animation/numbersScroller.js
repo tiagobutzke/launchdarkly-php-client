64ded813149e42be8175cc7fb41e16df5860a645
pavel.maximov <pavel.maximov@rocket-internet.de>
2015-05-29 11:47:55 +0200

    SGFD-15315 Cities animation speed fix and no teaser gradient

--- src/Volo/FrontendBundle/Resources/public/js/app/Animation/numbersScroller.js
+++ src/Volo/FrontendBundle/Resources/public/js/app/Animation/numbersScroller.js
@@ -5,10 +5,12 @@ var NumberScroller = function(scrollers, startingPointGetter, toNumber) {
         speedFactor = (110 - toNumber) / 100;
 
     function onScroll() {
+        var speedValue = 3 * speedFactor;
         if ((windowCache.scrollTop() + windowCache.height()) > startingPointGetter()) {
             scrollers.css({
                 top: '-' + (60 - toNumber) + '00%',
-                transition: 'top ' + (3 * speedFactor) + 's cubic-bezier(0.270, 1.170, 1.000, 1.000)'
+                '-webkit-transition': 'top ' + speedValue + 's cubic-bezier(0.270, 1.170, 1.000, 1.000)',
+                transition: 'top ' + speedValue + 's cubic-bezier(0.270, 1.170, 1.000, 1.000)'
             });
 
             windowCache.off('scroll', onScroll);
