5b882da7d7e97f5b643c948cab86ae8cfb5759f7
Riccardo Bini <riccardo.bini@rocket-internet.de>
2015-07-08 12:42:04 +0200

    SGFD-17203 city redirects fix (muenchen, koeln, duesseldorf)

--- src/Volo/FrontendBundle/Tests/Controller/RedirectTest.php
+++ src/Volo/FrontendBundle/Tests/Controller/RedirectTest.php
@@ -8,6 +8,29 @@ use Volo\FrontendBundle\Tests\VoloTestCase;
 class RedirectTest extends VoloTestCase
 {
     /**
+     * @dataProvider cmsDataProvider
+     *
+     * @param string $path
+     * @param string $target
+     */
+    public function testCms($path, $target)
+    {
+        $client = static::createClient();
+
+        $client->request('GET', $path);
+
+        $this->assertEquals(Response::HTTP_MOVED_PERMANENTLY,
+            $client->getResponse()->getStatusCode(),
+            sprintf('status code should be "%s", got "%s" for "%s"', Response::HTTP_MOVED_PERMANENTLY, $client->getResponse()->getStatusCode(), $path)
+        );
+
+        $this->assertTrue(
+            $client->getResponse()->isRedirect($target),
+            sprintf('Location should be "%s", got "%s" for "%s"', $target, $client->getRequest()->headers->get('Location'), $path)
+        );
+    }
+
+    /**
      * @dataProvider urlDataProvider
      *
      * @param string $path
@@ -28,6 +51,9 @@ class RedirectTest extends VoloTestCase
             $client->getResponse()->isRedirect($target),
             sprintf('Location should be "%s", got "%s" for "%s"', $target, $client->getRequest()->headers->get('Location'), $path)
         );
+
+        $client->followRedirect();
+        $this->isSuccessful($client->getResponse());
     }
 
     public function testLocation()
@@ -54,15 +80,30 @@ class RedirectTest extends VoloTestCase
     /**
      * @return array
      */
-    public function urlDataProvider()
+    public function cmsDataProvider()
     {
         return [
             ['/terms', 'http://localhost/contents/terms-and-conditions.htm'],
             ['/drivers', 'http://localhost/contents/drivers-and-restaurants.htm'],
             ['/restaurants', 'http://localhost/contents/drivers-and-restaurants.htm'],
             ['/imprint', 'http://localhost/contents/imprint.htm'],
-            ['/r/berlin', 'http://localhost/city/berlin'],
             ['/signup', 'http://localhost/login'],
+        ];
+    }
+    /**
+     * @return array
+     */
+    public function urlDataProvider()
+    {
+        return [
+            ['/r/berlin', 'http://localhost/city/berlin'],
+            ['/r/muenchen', 'http://localhost/city/muenchen'],
+            ['/r/koeln', 'http://localhost/city/koeln'],
+            ['/r/duesseldorf', 'http://localhost/city/duesseldorf'],
+
+            ['/city/munchen', 'http://localhost/city/muenchen'],
+            ['/city/koln', 'http://localhost/city/koeln'],
+            ['/city/dusseldorf', 'http://localhost/city/duesseldorf'],
         ];
     }
 }
