13d367476e811f6a5a1873021490785d90c20660
steffen.kraft <steffen.kraft@rocket-internet.de>
2015-07-15 14:46:22 +0200

    SGFD-18027 Hero-Banner images are not loaded

--- src/Volo/FrontendBundle/Dumper/ThumborConfigurationDumper.php
+++ src/Volo/FrontendBundle/Dumper/ThumborConfigurationDumper.php
@@ -29,11 +29,13 @@ class ThumborConfigurationDumper
         foreach ($this->transformations as $key => $transformer) {
             if (false !== strpos($key, 'bp_')) {
                 if (false === strrpos($key, '_retina')) {
-                    $content['breakpoints'][] = [
-                        'width' => $transformer['resize']['width'],
-                        'src' => 'data-src-' . $key,
-                        'mode' => 'viewport',
-                    ];
+                    if(false === strrpos($key, 'default')) {
+                        $content['breakpoints'][] = [
+                            'width' => $transformer['resize']['width'],
+                            'src' => 'data-src-' . $key,
+                            'mode' => 'viewport',
+                        ];
+                    }
                 }
             }
         }
