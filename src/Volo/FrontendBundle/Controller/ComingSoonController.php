814ec88caf83788b5807a4738e232f211bd7702e
Riccardo Bini <riccardo.bini@rocket-internet.de>
2015-08-07 12:33:02 +0200

    SGFD-18925 skip coming soon page with cookie

---
+++ src/Volo/FrontendBundle/Controller/ComingSoonController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Volo\FrontendBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ComingSoonController extends Controller
+{
+    /**
+     * @Route("/testing-mode", name="coming-soon-disable")
+     *
+     * @param Request $request
+     *
+     * @return array
+     */
+    public function homeAction(Request $request)
+    {
+        $response = new Response('Coming soon page disabled.');
+        $response->headers->setCookie(new Cookie('coming-soon-disabled', 'true', 0, '/', null, $request->isSecure(), false));
+
+        return $response;
+    }
+
+}
