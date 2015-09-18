c1c1c3a9b083afaed19e7e3eea05323fea3d257b
steffen.kraft <steffen.kraft@rocket-internet.de>
2015-09-18 12:18:03 +0200

    INTVOLO-821 Austria / Redirect Banner

---
+++ src/Volo/FrontendBundle/Resources/public/js/app/View/vendorPopupView.js
@@ -0,0 +1,16 @@
+VOLO.VendorPopupView = Backbone.View.extend({
+    initialize: function() {
+        _.bindAll(this);
+    },
+
+    render: function() {
+        this.$el.modal('show');
+
+        return this;
+    },
+
+    unbind: function() {
+        this.stopListening();
+        this.undelegateEvents();
+    }
+});
