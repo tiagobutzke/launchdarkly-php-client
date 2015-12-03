7f7f501802ee19e691b62e2ca0d7d876bf3ddc65
Maximilian Beyer <maximilian.beyer@foodora.com>
2015-12-03 16:45:42 +0100

    INTVOLO-1545 check if values are set before making request to server
    INTVOLO-1545 cleanup

--- src/Volo/FrontendBundle/Resources/public/js/app/Validators/emailApi.js
+++ src/Volo/FrontendBundle/Resources/public/js/app/Validators/emailApi.js
@@ -1,5 +1,9 @@
 VOLO.emailApiValidator = function (value) {
     return new validate.Promise(function (resolve, reject) {
+        if(!value) {
+            return reject('Email validation failed because null');
+        }
+
         $.ajax({
             url: Routing.generate('customer_validate_email', {email: encodeURIComponent(value)}),
             success: resolve,
