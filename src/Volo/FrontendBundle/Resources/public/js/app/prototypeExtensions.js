828a528cf267a58558312f814bae84f3217e1b1b
Maximilian Beyer <maximilian.beyer@foodora.com>
2015-12-18 18:08:53 +0100

    INTVOLO-1883 catch errors thrown by native getBoundingClientRect function

---
+++ src/Volo/FrontendBundle/Resources/public/js/app/prototypeExtensions.js
@@ -0,0 +1,14 @@
+(function () {
+    HTMLElement.prototype._getBoundingClientRect = HTMLElement.prototype.getBoundingClientRect;
+    HTMLElement.prototype.getBoundingClientRect = function () {
+        var box = {
+            top: this.offsetTop,
+            left: this.offsetLeft
+        };
+        try {
+            box = this._getBoundingClientRect();
+        } catch (e) {}
+
+        return box;
+    }
+})();
