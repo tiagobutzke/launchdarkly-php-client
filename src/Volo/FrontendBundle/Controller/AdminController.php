b19b9a48391807e7fc88648ed926d9822250ec25
Riccardo Bini <riccardo.bini@foodora.com>
2016-01-05 11:35:16 +0100

    INTVOLO-1909 Added cache for CMS

---
+++ src/Volo/FrontendBundle/Controller/AdminController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Volo\FrontendBundle\Controller;
+
+class AdminController
+{
+    // /admin/cms/delete?password=random_hash (hard-coded and environment specific)
+    // cmsService
+}
--- src/Volo/FrontendBundle/Service/CmsService.php
+++ src/Volo/FrontendBundle/Service/CmsService.php
@@ -3,6 +3,7 @@
 namespace Volo\FrontendBundle\Service;
 
 use Doctrine\Common\Cache\Cache;
+use Doctrine\Common\Cache\CacheProvider;
 use Foodpanda\ApiSdk\Entity\Cms\CmsItem;
 use Foodpanda\ApiSdk\Exception\EntityNotFoundException;
 use Foodpanda\ApiSdk\Provider\CmsProvider;
@@ -15,36 +16,60 @@ class CmsService
     private $cmsProvider;
 
     /**
-     * @var Cache
+     * @var CacheProvider
      */
     private $cache;
+
+    /**
+     * @var CacheProvider
+     */
+    private $cacheAll;
 
     /**
      * @param CmsProvider $cmsProvider
      * @param Cache $cache
      */
-    public function __construct(CmsProvider $cmsProvider, Cache $cache)
+    public function __construct(CmsProvider $cmsProvider, CacheProvider $cache, CacheProvider $cacheAll)
     {
         $this->cmsProvider = $cmsProvider;
         $this->cache = $cache;
+        $this->cacheAll = $cacheAll;
     }
 
     /**
      * @param string $code
-     * @param bool $caseSensitive
      *
      * @return CmsItem
      * @throws EntityNotFoundException
      */
-    public function findByCode($code, $caseSensitive = true)
+    public function findByCode($code)
     {
-        if (!$this->cache->contains($code)) {
-            $cmsItem = $this->cmsProvider->findByCode($code, $caseSensitive);
-            $this->cache->save($code, $cmsItem, 300); // temporary workaround
+        $code = strtolower($code);
+        $element = null;
+        if (!$this->cacheAll->contains('all')) {
+            $cmsItems = $this->cmsProvider->findAll();
 
-            return $cmsItem;
+            foreach ($cmsItems as $item) {
+                $this->cache->save(strtolower($item->getCode()), $item, 172800);
+            }
+
+            $this->cacheAll->save('all', 1, 172800);
         }
 
-        return $this->cache->fetch($code);
+
+        if ($this->cache->contains($code)) {
+            $element = $this->cache->fetch($code);
+        }
+
+        if (null === $element) {
+            throw new EntityNotFoundException();
+        }
+
+        return $element;
+    }
+
+    public function delete()
+    {
+        $this->cache->delete('');
     }
 }
